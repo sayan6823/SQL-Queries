e5d3dbbb5485205e448a2b1ec9d8defd222fdc47
sayan6823 <sayanchat6823@gmail.com>
2025-09-17 23:01:51 +0530

    fixed bugs

--- occupations.sql
+++ occupations.sql
@@ -1,26 +1,15 @@
-select 
-case when grade < 8 then NULL else name end as Name,
-grade,
-marks
-from students s join grades g
-on s.marks between g.min_mark and g.max_mark
-order by grade desc , name asc;
-
-
-
--- One more Solution
--- select
---  min(Doctor) Doctor, 
---  min(Professor) Professor,
---  min(Singer) Singer,
---  min(Actor) Actor
--- from
--- (select row_number() over(partition by occupation order by name) as row_numbr,
---  case when occupation ='Doctor' then name end as doctor,
---  case when occupation = 'Professor' then name end as professor,
---  case when occupation ='Singer' then name end as singer,
---  case when occupation = 'Actor' then name end as actor
---  from occupations
--- ) temp_table
--- group by row_numbr
--- order by row_numbr;
+select
+ min(Doctor) Doctor, 
+ min(Professor) Professor,
+ min(Singer) Singer,
+ min(Actor) Actor
+from
+(select row_number() over(partition by occupation order by name) as row_numbr,
+ case when occupation ='Doctor' then name end as doctor,
+ case when occupation = 'Professor' then name end as professor,
+ case when occupation ='Singer' then name end as singer,
+ case when occupation = 'Actor' then name end as actor
+ from occupations
+) temp_table
+group by row_numbr
+order by row_numbr;
